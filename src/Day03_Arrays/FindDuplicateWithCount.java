94a6e469e0d49daf2c07e170d41d2b7bd4812469
Anjali-2412 <naina.jain3333@gmail.com>
2025-06-08 18:23:43 +0530

    day03 added FindDuplicateWithCoun code

---
+++ src/Day03_Arrays/FindDuplicateWithCount.java
@@ -0,0 +1,21 @@
+package Day03_Arrays;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FindDuplicateWithCount {
+public static void main(String[] args) {
+	int[] arr = {1,1,1,1,4,5,3,2,2,2,2,4,5,3,2};
+	HashMap<Integer, Integer> countMap = new HashMap<Integer, Integer>();
+	//count frequency of each number
+	for(int num : arr) {
+		countMap.put(num, countMap.getOrDefault(num, 0)+1);
+	}
+	
+	System.out.println("duplicates elements are : ");
+	for(Map.Entry<Integer, Integer> entry : countMap.entrySet()) {
+		System.out.println("value: "+entry.getKey()+" count: "+entry.getValue());
+	}
+	
+}
+}
