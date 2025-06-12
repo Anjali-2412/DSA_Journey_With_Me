9e4a37e8e64073411c56a7a05cbd3d5c93186ef2
Anjali-2412 <naina.jain3333@gmail.com>
2025-06-12 22:06:37 +0530

    Day07: Added Code of Remove every second element from the array

---
+++ src/Day07/RemoveEverySecondElement.java
@@ -0,0 +1,23 @@
+package Day07;
+
+public class RemoveEverySecondElement {
+	public static void main(String[] args) {
+		int[] original = {10,20,30,40,50,60,70};
+		int newSize = (original.length+1)/2;
+		int[] result = new int[newSize];
+		
+		int index=0;
+		
+		for(int i=0;i<original.length;i+=2) {
+			result[index] = original[i];
+			index++;
+		}
+		
+		System.out.println("Array after removing every seond element : ");
+		for(int i=0;i<result.length;i++) {
+			System.out.print(result[i]+" ");
+		}
+		
+		
+	}
+}
