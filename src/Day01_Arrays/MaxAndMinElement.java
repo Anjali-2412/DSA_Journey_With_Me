6f12b0d5bfe01c158eff6d14d92a83ac1d5e2415
Anjali-2412 <naina.jain3333@gmail.com>
2025-06-12 22:10:11 +0530

    Day01 : Update code of find minimum element

--- src/Day01_Arrays/MaxAndMinElement.java
+++ src/Day01_Arrays/MaxAndMinElement.java
@@ -1,15 +1,20 @@
 package Day01_Arrays;
 
-public class MaxElement {
+public class MaxAndMinElement {
 public static void main(String[] args) {
 	int[] a= {12,23,45,65,100};
 	int max=a[0];
+	int min=a[0];
 	for(int i=1;i<a.length;i++) {
 		if(a[i]>max) {
 			max = a[i];
 		}
+		else if(a[i]<min) {
+			min=a[i];
+		}
 	}
 	
 	System.out.println("max element : "+max);
+	System.out.println("min element : "+min);
 }
 }
--- src/Day07/RemoveEverySecondElement.java
+++ src/Day07/RemoveEverySecondElement.java
@@ -4,11 +4,11 @@ public class RemoveEverySecondElement {
 	public static void main(String[] args) {
 		int[] original = {10,20,30,40,50,60,70};
 		int newSize = (original.length+1)/2;
-		int[] result = new int[newSize];
+		int[] result = new int[newSize]; 
 		
 		int index=0;
 		
-		for(int i=0;i<original.length;i+=2) {
+		for(int i=0;i<original.length;i+=2) { 
 			result[index] = original[i];
 			index++;
 		}
