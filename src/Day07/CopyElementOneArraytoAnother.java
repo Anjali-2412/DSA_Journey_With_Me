8921c9d38573977cde5fe21742f6c6dee2b5c836
Anjali-2412 <naina.jain3333@gmail.com>
2025-06-12 21:56:14 +0530

    Day07 : Added code of find Index of target element

---
+++ src/Day07/CopyElementOneArraytoAnother.java
@@ -0,0 +1,25 @@
+package Day07;
+
+public class CopyElementOneArraytoAnother {
+	public static void main(String[] args) {
+		
+       int[] original = {10,20,30,40,50};
+		
+		//Create another array with same length
+		
+		int[] copy= new int[original.length];
+		
+		//copy element manually
+		for(int i=0;i<original.length;i++) {
+			copy[i]=original[i];
+			
+		}
+		//display copied array
+		System.out.println("copied Array");
+		for(int i=0;i<copy.length;i++) {
+			System.out.print(copy[i]+" ");
+		}
+	
+	
+	}
+}
