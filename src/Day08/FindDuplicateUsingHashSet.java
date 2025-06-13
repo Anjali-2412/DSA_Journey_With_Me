08063d4ce3c2cda570b01fadd3a284eb7299802f
Anjali-2412 <naina.jain3333@gmail.com>
2025-06-14 01:30:23 +0530

    Day08 : Added code of Find duplicate using Hashset

--- src/Day08/FindDuplicateUsingHashSet.java
+++ src/Day08/FindDuplicateUsingHashSet.java
@@ -1,5 +1,31 @@
 package Day08;
 
-public class FindDuplicateUsingHashSet {
+import java.util.HashSet;
 
+public class FindDuplicateUsingHashSet {
+	public static void main(String[] args) {
+		int[] arr = {2,2,3,4,1,1,3,4,5,5};
+		HashSet<Integer> seen = new HashSet<Integer>();
+		HashSet<Integer> duplicate = new HashSet<Integer>();
+		
+		for(int num : arr) {
+			if(!seen.add(num)) {
+				duplicate.add(num);
+			}
+		}
+		
+		if(duplicate.isEmpty()) {
+			System.out.println("Duplicate values : None");
+		}else {
+		
+		System.out.println("Duplicates values : ");
+		for(int num : duplicate) {
+			System.out.print(num+" ");
+		}
+		}
+		
+		
+		//time complexity = o(1)
+		//space = o(n) as two hashset seen and duplicate
+	}
 }
