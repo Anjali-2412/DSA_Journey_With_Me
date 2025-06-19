343fc217189e963c33f049add6e5601445a9bebe
Anjali-2412 <naina.jain3333@gmail.com>
2025-06-19 15:19:57 +0530

    Day10 : added code of Find Missing no from list of elements

--- src/Day10/FindMissingNo.java
+++ src/Day10/FindMissingNo.java
@@ -1,7 +1,23 @@
 package Day10;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class FindMissingNo {
 	public static void main(String[] args) {
+		
+			int[] arr = {1,2,5,6};
+			int n=6;
+			Set<Integer> set = new HashSet<>();
+			for(int num : arr) {
+			set.add(num);
+			}
+			System.out.println("Missing no are : ");
+			for(int i=1;i<=n;i++) {
+				if(!set.contains(i)) {
+					System.out.println(i);
+				}
+			}
 		
 	}
 }
