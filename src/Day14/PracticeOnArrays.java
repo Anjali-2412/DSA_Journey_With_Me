710d80c37755ad2d052f47ce1a886ada4b478036
Anjali-2412 <naina.jain3333@gmail.com>
2025-06-25 20:46:18 +0530

    Day14 : Practice set on Array

---
+++ src/Day14/PracticeOnArrays.java
@@ -0,0 +1,18 @@
+package Day14;
+
+public class PracticeOnArrays {
+	public static void main(String[] args) {
+	/*classroom of 500 students  you have to store marks of these 500 students
+	2 options -:
+	create 500 variable and 
+	Use array	(recommended)
+	*/	
+	//There are 3 ways to create an array in Java.	
+	int[] marks = new int[500];
+	int[] mark;
+	mark =new int[5];
+	
+	int[] markk = {1,2,3,4,5};
+	System.out.println(markk);
+	}
+}
