e99b9f73b73e3589fb8ccc858e2a2451561c5005
Anjali-2412 <naina.jain3333@gmail.com>
2025-06-28 23:17:56 +0530

    Day17 : added code of Pallindrom String using two pointer approach

---
+++ src/Day17/PallindromString.java
@@ -0,0 +1,29 @@
+package Day17;
+
+public class PallindromString {
+	public static boolean checkPallindromString(String input) {
+		int left=0;
+		int right = input.length()-1;
+		while(left<right) {
+			if(input.charAt(left)!=input.charAt(right)) {
+				return false;
+			}
+			left++;
+			right--;
+		}
+		
+		
+		
+		return true;
+	}
+	
+	public static void main(String[] args) {
+		String input = "madam";
+		if(checkPallindromString(input)) {
+			System.out.println("pallindrom String");
+		}
+		else {
+			System.out.println("not pallindrom String");
+		}
+	}
+}
