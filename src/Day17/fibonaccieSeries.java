0d73f5897bda754c5ccee79c9a7d8372b61b4ca5
Anjali-2412 <naina.jain3333@gmail.com>
2025-06-28 23:32:06 +0530

    day17: added code of fibonaccie series

---
+++ src/Day17/fibonaccieSeries.java
@@ -0,0 +1,13 @@
+package Day17;
+
+public class fibonaccieSeries {
+	public static void main(String[] args) {
+		int n=10,first =0,second =1;
+		for(int i=0;i<=n;++i) {
+			System.out.println(first+ " ");
+			int next = first+second;
+			first = second;
+			second =next;
+		}
+	}
+}
