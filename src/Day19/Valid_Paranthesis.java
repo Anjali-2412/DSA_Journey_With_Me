fbe38bbabb9f105384e13a71e91d5360633bfc56
Anjali-2412 <naina.jain3333@gmail.com>
2025-07-04 20:24:45 +0530

    day19: added code of Valid_Parenthesis

---
+++ src/Day19/Valid_Paranthesis.java
@@ -0,0 +1,36 @@
+package Day19;
+
+import java.util.Stack;
+
+public class Valid_Paranthesis {
+	public static boolean isValid(String s) {
+		Stack<Character> stack = new Stack<>();
+		for(char ch : s.toCharArray()) {
+			if(ch == '(' || ch == '[' || ch == '{') {
+				stack.push(ch);
+			}
+			else {
+				if(stack.isEmpty()) {
+					return false;
+				}
+				char top = stack.pop();
+				if( (ch == ')' && top != '(') ||
+					(ch == '}'  && top != '{') ||
+					(ch == ']' && top != '[')
+			      )
+				{
+					return false;
+				}
+			}
+		}
+		return stack.isEmpty();
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(isValid("()"));      //true
+		System.out.println(isValid("()[]{}"));   // true
+        System.out.println(isValid("(]"));       // false
+        System.out.println(isValid("([)]"));     // false
+        System.out.println(isValid("({[]})"));   // true
+	}
+}
