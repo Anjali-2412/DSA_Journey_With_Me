6a51f5d9782381743ed3dc936779eb35fca8f8ce
Anjali-2412 <naina.jain3333@gmail.com>
2025-08-03 22:21:41 +0530

    code of sort list of Emp by salary

--- src/Day26/SortListOfEmpBySalary.java
+++ src/Day26/SortListOfEmpBySalary.java
@@ -1,5 +1,24 @@
 package Day26;
 
-public class SortListOfEmpBySalary {
+import java.util.Arrays;
+import java.util.List;
 
+class Emp{
+	int id;String name;double sal;
+	public Emp(int id, String name, double sal){
+		this.id=id;
+		this.name=name;
+		this.sal=sal;
+	}	
+}
+public class SortListOfEmpBySalary {
+	public static void main(String[] args) {
+		List<Emp> list = Arrays.asList(
+				new Emp(1,"Ram",20000),
+				new Emp(2,"Shyam",15000),
+				new Emp(3,"Mohan",30000)
+				);
+		list.sort((e1,e2)->(int)(e1.sal-e2.sal));
+		list.forEach(e->System.out.println(e.name+" "+e.sal));
+	}
 }
