f8a3e19d5e24f99f28afd6e716e79aae71045346
Anjali-2412 <naina.jain3333@gmail.com>
2025-08-03 22:26:45 +0530

    code of singleton class

--- src/Day26/Singleton.java
+++ src/Day26/Singleton.java
@@ -1,5 +1,22 @@
 package Day26;
-
+class Singleton1{
+	private static  Singleton1 instance;
+	private Singleton1() {
+		System.out.println("instance has created");
+	}
+	public static Singleton1 getInstance() {
+		if(instance == null) {
+			instance =new Singleton1();
+		}
+		return instance;
+	}
+}
 public class Singleton {
-
+	public static void main(String[] args) {
+		Singleton1 obj = Singleton1.getInstance();
+		Singleton1 obj1 = Singleton1.getInstance();
+		System.out.println(obj1.equals(obj));
+		
+	}
+	
 }
