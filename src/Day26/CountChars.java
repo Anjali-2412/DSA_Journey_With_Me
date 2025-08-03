d4f391bd7e2975a3354da11f4ec94b03690f848c
Anjali-2412 <naina.jain3333@gmail.com>
2025-08-03 22:15:40 +0530

    added code of count char

--- src/Day26/CountChars.java
+++ src/Day26/CountChars.java
@@ -1,5 +1,14 @@
 package Day26;
 
-public class CountChars {
+import java.util.HashMap;
 
+public class CountChars {
+		public static void main(String[] args) {
+			String s = "aabccc";
+			HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+			for(char c : s.toCharArray()) {
+				map.put(c, map.getOrDefault(c, 0)+1);
+			}
+			System.out.println(map);
+		}
 }
