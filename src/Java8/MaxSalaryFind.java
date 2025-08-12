0b26a89473f4037f2a42ebec068c9d313a8c4d63
Anjali-2412 <naina.jain3333@gmail.com>
2025-08-12 17:36:04 +0530

    Add program of find Max salary of Employee having id, name ,salary using Streams JAVA -8

---
+++ src/Java8/MaxSalaryFind.java
@@ -0,0 +1,53 @@
+package Java8;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+class Employee{
+	String name;
+	int id;
+	double salary;
+	public Employee(String name, int id, double salary) {
+		super();
+		this.name = name;
+		this.id = id;
+		this.salary = salary;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public double getSalary() {
+		return salary;
+	}
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+	
+	
+	
+}
+public class MaxSalaryFind {
+		public static void main(String[] args) {
+			List<Employee> emp = Arrays.asList(
+					new Employee("John", 1, 1000),
+					new Employee("Mike", 2, 1500),
+					new Employee("Sam", 3, 1200)
+					);
+			Employee maxSalaryEmp = emp.stream().max(Comparator.comparingDouble(Employee::getSalary))
+					.orElse(null);
+			System.out.println("Max Salaried employee : "+maxSalaryEmp.getName()+" - "+maxSalaryEmp.getSalary());
+			
+			
+			
+		}
+}
