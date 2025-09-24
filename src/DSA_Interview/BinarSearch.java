509c3555ee1bda7df0fc239e3dddf0b2158aa5f2
Anjali-2412 <naina.jain3333@gmail.com>
2025-09-24 22:40:35 +0530

    added ques asked in interview : Binary Search

---
+++ src/DSA_Interview/BinarSearch.java
@@ -0,0 +1,29 @@
+package DSA_Interview;
+
+public class BinarSearch {
+	public static int Search(int arr[],int target) {
+		int left =0,right =arr.length-1;
+		
+		while(left<=right) {
+			int mid = left + (right - left)/2;
+			if(arr[mid]==target) {
+				return mid;
+			}
+			else if(arr[mid]<target){
+				left=mid+1;
+			}
+			else {
+				right=mid-1;
+			}
+		}
+	
+		return -1;
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		int arr[]= {1,2,3,4,5,6};
+		System.out.println(Search(arr,4));
+	}
+}
